697200c4a0608feaca48577756d4d1b3631c1669
dollarp-code <husseindollarp@yahoo.com>
2024-06-01 15:41:57 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import data from './data';
+import List from './List';
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+  // const clearList = () => {
+  //   setPeople([]);
+  // };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button className="btn btn-block" onClick={() => setPeople([])}>
+          clear list
+        </button>
+      </section>
+    </main>
+  );
+};
+export default App;
